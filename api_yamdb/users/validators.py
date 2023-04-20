9b54083848686f807dec2a18fe72edb5ed7d72a6
Дмитрий Бессмертный <ya@bessmerti.ru>
2023-04-20 16:38:30 +0300

    elif

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -15,7 +15,7 @@ def validate_username(username):
             f'В username допустимо использовать только буквы, цифры и '
             f'знаки @.+-_. Применение {restricted_symbols} запрещено.'
         )
-    elif username == RESTRICTED_USERNAME:
+    if username == RESTRICTED_USERNAME:
         raise ValidationError(
             f'Использовать имя {RESTRICTED_USERNAME}'
             'в качестве username запрещено.'
