0aed1fb749cf27c989da17db62defa8da89752eb
Дмитрий Бессмертный <ya@bessmerti.ru>
2023-04-25 12:47:52 +0300

    redoc

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
-                                   SpectacularSwaggerView)
+
 
 urlpatterns = [
     path('api/', include('api.urls'), name='api'),
@@ -11,10 +10,5 @@ urlpatterns = [
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
-    ),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/',
-         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/',
-         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    )
 ]
